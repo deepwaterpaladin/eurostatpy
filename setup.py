2cd22bd67b13445d7f91cfbba9bba170d8333734
Rielly Young <rielly.young11@gmail.com>
2024-08-07 10:51:12 -0400

    v.0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eurostatpy',
-    version='0.9.3',
+    version='0.9.4',
     description='Basic package for querying & downloading EuroStat data by dataset name or ID.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
