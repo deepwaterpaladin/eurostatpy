669531994da36151264437c6320598c52280c4b6
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-08-07 08:11:22 -0400

    Delete jsonstatpy directory

--- jsonstatpy/utility.py
+++
@@ -1,9 +0,0 @@
-def lst2html(lst):
-    html = "<table>"
-    for r in lst:
-        html += "<tr>"
-        for c in r:
-            html += "<td>{}</td>".format(c)
-        html += "</tr>"
-    html += "</table>"
-    return html
