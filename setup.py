9716f9b1f65e9c81276df006b0212c093b96cf1e
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-08-07 09:23:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='eurostatpy',
-    version='0.4.1',
+    version='0.5.1',
     description='Basic package for querying & downloading EuroStat data by dataset name or ID.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=['pandas', 'terminaltables']
+    install_requires=['pandas', 'terminaltables', 'aiohttp']
 )
